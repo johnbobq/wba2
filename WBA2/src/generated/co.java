77761e6b9f97ec115913c89bddf997ab4964fbc5
johnbobq <jq@in2web-design.de>
2013-04-13 20:35:33 +0200

    github einbindung

---
+++ WBA2/src/generated/co.java
@@ -0,0 +1,15 @@
+package generated;
+
+import generated.Rezept.Kommentieren;
+
+public class co {
+	
+	  // FŸge einen neuen Kommentar hinzu
+    Kommentieren k = new Kommentieren();
+    //  k.setName("34");
+    //k.setDatum("ICH");
+    //k.setZeit("http://nichts.com/foto.png");
+    k.setKommentare("wer wo hier");
+    um.getKommentare().add(k);
+
+}
